a02954849aa39d6af7ee81203f8c4b775009dd17
Suhaan0810 <109020801+Suhaan0810@users.noreply.github.com>
2025-04-20 17:21:27 +0530

    Update qcsim.py

--- qcsim.py
+++ qcsim.py
@@ -10,15 +10,14 @@ def operators(no_of_qubits, X_POS, Z_POS, H_POS):
     O=[[1]]
     for i in range(no_of_qubits):
         if i in X_POS:
-            O=tp(O,X)
+            O=tp(X,O)
         elif i in Z_POS:
-            O=tp(O,Z)
+            O=tp(Z,O)
         elif i in H_POS:
-            O=tp(O,H)
+            O=tp(H,O)
         else :
-            O=tp(O,I)
+            O=tp(I,O)
     return O
-
 def C_NOT(no_of_qubits, ctrl_list, ctrld_list):
     #ctrl_list is the list containing the indexes of the control qubits
     #ctrld_list is the list containing the indexex of the controlled qubits
@@ -28,17 +27,17 @@ def C_NOT(no_of_qubits, ctrl_list, ctrld_list):
         list_i = list(map(int,list(bin(i)[2:].zfill(no_of_qubits))))
         and_gate_result = 1
         for j in ctrl_list:
-            and_gate_result = and_gate_result * list_i[j]
+            and_gate_result = and_gate_result * list_i[no_of_qubits-1-j]
         if and_gate_result == 1:
             for k in ctrld_list:
-                list_i[k] = (list_i[k]+1)%2
+                list_i[no_of_qubits-1-k] = (list_i[no_of_qubits-1-k]+1)%2
             i_dash = 0
             for k in range(no_of_qubits):
                 i_dash += 2**(no_of_qubits-1-k) * list_i[k]
             C[i][i_dash] = 1
         else:
             C[i][i] = 1
-    return C, clen    
+    return C, clen
     
 def tp(t1, t2):
     m1, n1, m2, n2 = len(t1[0]), len(t1), len(t2[0]), len(t2)
@@ -79,22 +78,20 @@ class System:
         if i == 0:
             fop = op
         else:
-            fop = I2
+            fop = I
             for _ in range(i - 1):
-                fop = tp(fop, I2)
+                fop = tp(fop, I)
             fop = tp(fop, op)
         self.state = tp(fop, self.state)
 
 
 def black_box(no_of_qubits,a):
     operator_position=list(bin(a)[2:].zfill(no_of_qubits))
-    O_=[[1]]
-    for _ in range(no_of_qubits):
-        if operator_position[no_of_qubits-1-_]=='0':
-            O_=tp(O_,X)
-        else :
-            O_=tp(O_,I)
-    O_=tp(O_,I)
+    X_POS=[]
+    for _ in operator_position:
+        if _ == '0':
+            X_POS.append(no_of_qubits-1-operator_position.index(_))
+    O_=operators(no_of_qubits+1,X_POS,[],[])
     C, clen=C_NOT(no_of_qubits+1,list(range(no_of_qubits)),[no_of_qubits])
     O=[[0 for _ in range(clen)]for __ in range(clen)]
     for i in range(clen):
